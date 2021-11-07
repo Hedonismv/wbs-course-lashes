88a1dff9bb9f4c81277750afbac8e979de8fd5a3
Vitaly Klyhun <klygun90@gmail.com>
2021-11-07 21:05:27 +0300

    end calc

--- src/components/Calculator/Calculator.js
+++ src/components/Calculator/Calculator.js
@@ -1,4 +1,5 @@
 import React, {useState} from "react";
+import style from './Calculator.module.css';
 
 
 const Calculator = (props) => {
@@ -8,8 +9,10 @@ const Calculator = (props) => {
     const [firstPayment, setFirstPayment] = useState('')
     const [error, setError] = useState(false)
     const [loading, setLoading] = useState(false)
+    const [isCalculated, setIsCalculated] = useState(false)
 
     const handleMonthChange = (event) => {
+        setIsCalculated(false)
         setMonth(event.target.value)
         if(/\D/.test(event.target.value)){
             setError(true)
@@ -21,6 +24,7 @@ const Calculator = (props) => {
         }
     }
     const handleFirstPaymentChange = (event) => {
+        setIsCalculated(false)
         setFirstPayment(event.target.value)
         if(/\D/.test(event.target.value)){
             setError(true)
@@ -39,6 +43,7 @@ const Calculator = (props) => {
         const result = (+cleanPrice * percent) / 100 + +cleanPrice
         setLoading(false)
         setCalcValue(result)
+        setIsCalculated(true)
 
 
     }
@@ -53,14 +58,21 @@ const Calculator = (props) => {
         }
     }
     return (
-        <form onSubmit={handleSubmit}>
-            <input type='text' placeholder='Сумма курса' disabled value={props.price} />
-            <input disabled={loading} type='text' placeholder='Количество месяцев от 1 до 6' value={month} onChange={handleMonthChange}/>
-            <input disabled={loading} type='text' placeholder='Сумма первого взноса' value={firstPayment} onChange={handleFirstPaymentChange}/>
-            {error ? <span style={{color: "red"}}>Введены неправильные данные!</span> : null}
-            <input type='submit' disabled={loading} value='Рассчитать'/>
-            <span>{calcValue}</span>
-        </form>
+        <div>
+            <form className={style.formCalcMain} onSubmit={handleSubmit}>
+                <input type='text' placeholder='Сумма курса' disabled value={props.price} />
+                <input disabled={loading} type='text' placeholder='Количество месяцев от 1 до 6' value={month} onChange={handleMonthChange}/>
+                <input disabled={loading} type='text' placeholder='Сумма первого взноса' value={firstPayment} onChange={handleFirstPaymentChange}/>
+                {error ? <span className={style.error}>Введены неправильные данные!</span> : null}
+                <input className={style.submitInput} type='submit' disabled={loading} value='Рассчитать'/>
+            </form>
+            {isCalculated ?
+                <div className={style.resultBlock}>
+                    <div>Cумма ежемесячного платежа:&nbsp;{calcValue}&nbsp;Р</div>
+                    <div>Количество месяцев:&nbsp;{month}</div>
+                </div> : null
+            }
+        </div>
     )
 }
 
